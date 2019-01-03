94ae5159b8455e947ec48a2a1894fe0cbf9b40ef
curiositypaths <curiositypaths@users.noreply.github.com>
2019-01-03 14:06:30 -0500

    Vanilla JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    attendees: [],
+    name: ""
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <input
+          type="text"
+          onChange={event =>
+            this.setState({
+              name: event.target.value
+            })
+          }
+          value={this.state.name}
+        />
+        <button
+          onClick={() => {
+            this.setState({
+              name: "",
+              attendees: [...this.state.attendees, this.state.name]
+            });
+          }}
+        >
+          Attend
+        </button>
+        <p>Attending the Redux party: {this.state.attendees.join(", ")}</p>
+      </>
     );
   }
 }
